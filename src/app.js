832592f3b98ed1537cc3c45eb2a756f3c0af58e6
ayu3456 <coaderayush1309@gmail.com>
2025-04-23 01:39:59 +0530

    middlewares done

--- src/app.js
+++ src/app.js
@@ -1,25 +1,36 @@
 // create server
-
 const express = require("express");
-
+const { adminAuth,userAuth } = require("./middlewares/auth");
 const app = express();
 
-// get /users => middleware chain => request handler 
 
-app.get("/user", (req, res, next) => {
-  console.log("Handling the route");
-  next();
-  res.send("response 1"); // send the response at this point , it will not go any further
+
+// handle auth middleware for all request handlers
+app.use("/admin", adminAuth);
+
+
+app.get("/user", userAuth, (req, res, next) => {
+  res.send("user data send");
 });
 
-app.get("/user", (req, res, next) => {
-  console.log("Handling the route user2");
-  res.send("2nd route handler"); // this works exactly the same way
-}); // the functions u put in btw are called middlewares.
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
 
-// if u are using next and res both it will throw an error that u cant send the res twice.
+app.get("/admin/deleteAllData", (req, res) => {
+  const token = "xyzh";
+  const isAdminAuth = token === "xyz";
 
-//you are creating a webserver , u have to listen to the upcoming request.
+  if (isAdminAuth) {
+    res.send("delete the user data");
+  } else {
+    res.status(401).send("admin not Authorised"); // to avoid repitition middleware come into the picture.
+  }
+});
+
+// we should check the auth of the admin . should i check the auth of admin everywhere.
+// validate the token.
+
 app.listen(3000, () => {
   console.log("Server is successfully running on port 3000");
 }); // curson is waiting and listening on the server.
