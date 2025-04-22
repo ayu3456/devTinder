26889ef4c9db8b5c95130f8aa28fb4d6b5519e07
ayu3456 <coaderayush1309@gmail.com>
2025-04-22 20:35:04 +0530

    multiple route handler again syntax change

--- src/app.js
+++ src/app.js
@@ -10,16 +10,10 @@ app.get("/user", (req, res, next) => {
   res.send("response 1"); // send the response at this point , it will not go any further
 });
 
-app.get('/user',(req,res,next) => {
-    console.log("Handling the route user2")
-    res.send("2nd route handler") // this works exactly the same way 
-})
-
-
-
-
-
-
+app.get("/user", (req, res, next) => {
+  console.log("Handling the route user2");
+  res.send("2nd route handler"); // this works exactly the same way
+}); // the functions u put in btw are called middlewares.
 
 // if u are using next and res both it will throw an error that u cant send the res twice.
 
