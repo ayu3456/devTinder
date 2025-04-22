74f4f7fbbb984e6b47881f71d52ab492d7256e99
ayu3456 <coaderayush1309@gmail.com>
2025-04-22 19:58:28 +0530

    multiple route handler

---
+++ Notes/episode-05.md
@@ -0,0 +1,2 @@
+# middlewares and routes handlers 
+
--- src/app.js
+++ src/app.js
@@ -2,42 +2,35 @@
 
 const express = require("express");
 
-const app = express(); // this is the instace of an express js application.
+const app = express();
 
-// whatever reqest is coming in. it is responding the same way it is.
-//but if u give route then it will give outcome only on that route eg:Test.
-
-app.use("/test", (req, res) => {
-  res.send("I am going to test the app");
-}); // this route will match all the http method API Call to /test.
-
-app.get("/user", (req, res) => {
-  console.log(req.query);
-
-  res.send({
-    firstname: "Ayush",
-    lastname: "Gupta",
-  });
-}); // this will only handle get call to user.
-
-// handle dynamic userid
-
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({
-    firstname: "Kush",
-    lastname: "Agarwal",
-  });
+app.use("/user", (req, res,next) => {
+  console.log("Handling the route");
+  next();
+  res.send("response 1"); // send the response at this point , it will not go any further 
+  
+},(req,res)=>{
+    console.log("Handling the route 2");
+    res.send("Response 2");
 });
 
-app.post("/user", (req, res) => {
-  console.log("Save data to the database");
-  res.send("Data is saved");
-});
+// if u are using next and res both it will throw an error that u cant send the res twice. 
 
-app.delete("/user", (req, res) => {
-  res.send("User Delete");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //you are creating a webserver , u have to listen to the upcoming request.
 app.listen(3000, () => {
