4bee6e6db91df116de3e2c4c8a02db2a273b6a1c
ayu3456 <coaderayush1309@gmail.com>
2025-04-22 20:11:15 +0530

    multiple route handler again

--- src/app.js
+++ src/app.js
@@ -4,33 +4,32 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res,next) => {
-  console.log("Handling the route");
-  next();
-  res.send("response 1"); // send the response at this point , it will not go any further 
-  
-},(req,res)=>{
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route");
+    next();
+    //res.send("response 1"); // send the response at this point , it will not go any further
+  },
+  (req, res,next) => {
     console.log("Handling the route 2");
-    res.send("Response 2");
-});
-
-// if u are using next and res both it will throw an error that u cant send the res twice. 
-
-
-
-
-
-
-
-
-
-
+    //res.send("Response 2");
+    next()
+  },
+  (req, res,next) => {
+    console.log("Handling the route 3");
+    //res.send("Response 3");
+    next()
+  },
+  (req, res,next) => {
+    console.log("Handling the route 4");
+    res.send("Response 4");
+  }
 
 
+);
 
-
-
-
+// if u are using next and res both it will throw an error that u cant send the res twice.
 
 //you are creating a webserver , u have to listen to the upcoming request.
 app.listen(3000, () => {
