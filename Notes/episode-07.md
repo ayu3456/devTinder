1ade44e3d4d35c41341c87b3dafdf61bf7cded27
ayu3456 <coaderayush1309@gmail.com>
2025-04-23 18:05:12 +0530

    get api

---
+++ Notes/episode-07.md
@@ -0,0 +1,15 @@
+# Deeping dive into database 
+I want to send data from the postman to the database now. earlier we are doing hard coded data. 
+
+difference btw json and js-object.
+
+req aayi -> tumne console.log(req.body) kiya to undefined aaya. kyu  ye req ko parse nahi kar pa raha tha. 
+uske liye middleware chaiye hota hai. 
+express.json 
+
+now we are going to get the user from the db. feed 
+
+
+
+
+
--- src/app.js
+++ src/app.js
@@ -4,25 +4,59 @@ const connectdb = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json()); // it will activated for all the routes  It read the req and convert into a js object.
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "virat",
-    lastName: "kohli",
-    emailId: "virat@gmail.com",
-    password: "virat@123",
-  };
+  //console.log(req.body);
+
+  const userObj = req.body;
 
   // Creating a new Instance of the user model
   const user = new User(userObj);
 
   try {
     await user.save(); // this return us a promise
-    res.send("user added successfully");
+    res.send("User added successfully");
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error saving the user:" + err.message);
+    res.status(500).send("Error saving the User:" + err.message);
   }
 });
+
+// get user from email
+app.get("/user", async (req, res) => {
+  //console.log(req.body.email)
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+
+    if (user.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Cannot get the req from the User");
+  }
+});
+
+// Feed api /feed get all user from the database.
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    //console.log(users)
+    if (users.length === 0) {
+      res.status(400).send("users not found");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Cannot get the req from the User");
+  }
+});
+
+
 
 connectdb()
   .then(() => {
