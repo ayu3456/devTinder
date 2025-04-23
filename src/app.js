1d31ee3cc005004f4fcefc6a3171caf52284cf12
ayu3456 <coaderayush1309@gmail.com>
2025-04-23 12:08:57 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,36 +1,26 @@
 // create server
 const express = require("express");
-const { adminAuth,userAuth } = require("./middlewares/auth");
 const app = express();
 
+app.get("/getUserData", (req, res) => {
+  try {
+    // Logic of DB Call and get some user data agar error hai to try - catch block ka use karte hai
+    throw new Error("Kuch kam nahi ho raha");
 
-
-// handle auth middleware for all request handlers
-app.use("/admin", adminAuth);
-
-
-app.get("/user", userAuth, (req, res, next) => {
-  res.send("user data send");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data sent");
-});
-
-app.get("/admin/deleteAllData", (req, res) => {
-  const token = "xyzh";
-  const isAdminAuth = token === "xyz";
-
-  if (isAdminAuth) {
-    res.send("delete the user data");
-  } else {
-    res.status(401).send("admin not Authorised"); // to avoid repitition middleware come into the picture.
+    res.send("User data sent");
+  } catch (err) {
+    res.status(500).send("Fir se fat gaya");
   }
 });
 
-// we should check the auth of the admin . should i check the auth of admin everywhere.
-// validate the token.
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    //log error
+
+    res.status(500).send("Error aa gaya bro!");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully running on port 3000");
-}); // curson is waiting and listening on the server.
+});
